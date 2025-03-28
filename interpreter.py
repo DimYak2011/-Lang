796f2c5e2e5d5713c7c1ad6172831ba7fb1c6f34
DimYak2011 <dmitryak2011@gmail.com>
2025-03-28 17:21:47 +0300

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,37 @@
+def run_code(cmd):
+    translations = {
+        'абоюдна': 'print',
+        'обидно': 'else',
+        'абидна': 'if',
+        'абаюдно': 'input',
+        'абаюнда': 'for',
+        'абоюнда': 'while'
+    }
+
+
+    for key, value in translations.items():
+        cmd = cmd.replace(key, value)
+
+    try:
+        exec(cmd)
+    except Exception as e:
+        print("Вы инвалид")
+
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            cmd = input(">>> ")
+        except EOFError:
+            print("\nВыход")
+            break
+        except KeyboardInterrupt:
+            print("\nВыход")
+            break
+
+        if cmd == "выход":
+            break
+
+        run_code(cmd)
+
